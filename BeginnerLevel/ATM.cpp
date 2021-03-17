e50dfcde91625621ddde1cd4ad6e31b9810ec5bf
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-03-17 13:29:46 +0530

    done

--- BeginnerLevel/ATM.cpp
+++ BeginnerLevel/ATM.cpp
@@ -1,28 +1,31 @@
+
+
+// OPTIMIZE WAY
+
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
 
-    cout << "Enter total money with 2 decimal point : ";
     int with_money;
-    float tot_money;
+    float tot_money; 
     cin >> with_money >> tot_money;
 
-    while (with_money < tot_money)
-    {
+    if(with_money >= 0 && with_money <= 2000 && tot_money >= 0 && tot_money <= 2000){
+        if (with_money+0.5 > tot_money)
+        {
+            printf("%.2f\n", tot_money);
+        }
         /* code */
-        if (with_money % 5 == 0)
+        else if (with_money % 5 == 0)
         {
             tot_money = tot_money - (with_money + 0.5);
-            printf("%.2f", tot_money);
+            printf("%.2f\n", tot_money);
         }
-        else if (with_money % 5 != 0)
+        else
         {
-            printf("%.2f", tot_money);
+            printf("%.2f\n", tot_money);
         }
     }
-    printf("%.2f", tot_money);
-
     return 0;
 }
