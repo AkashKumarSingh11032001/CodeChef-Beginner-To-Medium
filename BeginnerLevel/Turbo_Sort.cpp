630e13139d10f3ee1423efb3e7f0757e326e88f3
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-03-17 16:38:57 +0530

    Day1: Question8

---
+++ BeginnerLevel/Turbo_Sort.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, arr + size);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
