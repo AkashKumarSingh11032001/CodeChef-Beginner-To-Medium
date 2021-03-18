2b46380058c85846256dd4d33dc6ec5040d62399
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-03-18 09:37:53 +0530

    Day2: Qestion1

---
+++ BeginnerLevel/Finding_Square_Roots.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int T;
+    cin>>T;
+
+    while (T>0)
+    {
+        /* code */
+        int n;
+        cin>>n;
+        cout << (int)sqrt(n)<<endl;
+        T--;
+    }
+    
+}
